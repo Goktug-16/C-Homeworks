6b6e81301b4f9ee885559b3dba9350e0df447ed3
Goktug-16 <151882979+Goktug-16@users.noreply.github.com>
2024-09-03 12:57:45 +0300

    Add files via upload

---
+++ ÇARPIM TABLOSU.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Lütfen bir sayı girin:");
+                int sayi = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{sayi} x {i} = {sayi * i}");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Lütfen geçerli bir sayı giriniz.");
+            }
+        }
+    }
+}
