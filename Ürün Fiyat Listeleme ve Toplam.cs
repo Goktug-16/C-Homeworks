5e839341635df4efb24f209eddc9ac0bc13f288b
Goktug-16 <151882979+Goktug-16@users.noreply.github.com>
2024-09-11 13:20:51 +0300

    Add files via upload

---
+++ Ürün Fiyat Listeleme ve Toplam.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal toplamFiyat = 0;
+            int urunSayisi = 0;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Kaç adet ürün gireceksiniz ?");
+                    urunSayisi = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Geçerli bir sayı giriniz.");
+                }
+            } 
+            while (urunSayisi <= 0);
+
+            int sayac = 0;
+
+            while (sayac < urunSayisi)
+            {
+                try
+                {
+                    Console.WriteLine($"{sayac + 1}. ürünün fiyatını giriniz:");
+                    decimal fiyat = Convert.ToDecimal(Console.ReadLine());
+                    toplamFiyat += fiyat;
+                    sayac++;
+                }
+                catch
+                {
+                    Console.WriteLine("Geçerli bir fiyat giriniz.");
+                }
+            }
+            Console.WriteLine($"Toplam ürün fiyatı: {toplamFiyat} TL");
+        }
+    }
+}
